c457fe78d0c28c7d04051f670bbd25c58e50ee9c
James Jamieson <JamesJamieson18@gmail.com>
2021-09-21 13:40:07 -0700

    Updated IP

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.117.151.46',
+      host: '3.133.111.196',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
